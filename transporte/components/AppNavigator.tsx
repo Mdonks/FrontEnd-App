c43c5183479210e79a574da0d4ee4818d5e6f826
Mdonks <137445961+Mdonks@users.noreply.github.com>
2024-06-13 20:01:34 -0600

    commit 6

--- transporte/components/AppNavigator.tsx
+++ transporte/components/AppNavigator.tsx
@@ -1,4 +1,3 @@
-// AppNavigator.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
